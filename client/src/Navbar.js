1f70dc4fef640d8475ba5186a4f4db32976b4310
Phil Todisco <philtodiscoglass@gmail.com>
2023-02-28 20:09:42 -0500

    Navbar layout

---
+++ client/src/Navbar.js
@@ -0,0 +1,13 @@
+export const Navbar = () => {
+  return <nav className="nav">
+    <a href="/" className="site-title">Dead To The Core</a>
+    <ul>
+        <li>
+            <a href="/band">Band</a>
+            <a href="/tour">Tour</a>
+            <a href="/gallery">Gallery</a>
+            <a href="/contact">Contact</a>
+        </li>
+    </ul>
+    </nav>
+}
