2d30fd2f08877ea3f1ea63d0bfbecc18b9b0012d
Phil Todisco <philtodiscoglass@gmail.com>
2023-03-02 20:18:08 -0500

    Add navbar routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react'
-import Home from "./pages/Home"
+import Home from './pages/Home'
+import Band from './pages/Band'
+import Tour from './pages/Tour'
+import Gallery from './pages/Gallery'
+import Contact from './pages/Contact'
 import { Navbar } from './Navbar'
 import { Route, Routes } from "react-router-dom"
 
@@ -9,6 +13,10 @@ export default function App() {
   <Navbar />
     <Routes>
       <Route path = "/" element = {<Home />} />
+      <Route path = "/band" element = {<Band />} />
+      <Route path = "/tour" element = {<Tour />} />
+      <Route path = "/gallery" element = {<Gallery />} />
+      <Route path = "/contact" element = {<Contact />} />
     </Routes>
   </>
   )
