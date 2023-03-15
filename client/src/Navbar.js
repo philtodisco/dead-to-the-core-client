48889d75014efb746293a9b5a25b52bad7556c38
Phil Todisco <philtodiscoglass@gmail.com>
2023-03-14 21:19:56 -0400

    Update navbar, remove hero image

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom"
+import Logo from "./images/dttc-logo.png"
 
 export const Navbar = () => {
   return <nav className="nav">
-    <Link to ="/" className="site-title">Dead To The Core</Link>
+    <Link to ="/" className="site-title"> 
+      <img id="logo" src={Logo}
+      alt="rock concert"/>
+    </Link>
     <ul className="nav-links">
             <Link to ="/band">Band</Link>
             <Link to ="/tour">Tour</Link>
@@ -11,3 +15,4 @@ export const Navbar = () => {
     </ul>
     </nav>
 }
+
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,4 @@
 export default function Home() {
-  return <div class="hero-image">
-  <div class="hero-text">
-    <img class="logo" src="https://i.imgur.com/shKmshK.jpg" alt="Italian Trulli"></img>
-  </div>
-</div>
+  return 
 }
 
