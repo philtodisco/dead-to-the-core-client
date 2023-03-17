9db5493d9ac362b99557f2cb422a084cfc66c825
Phil Todisco <philtodiscoglass@gmail.com>
2023-03-16 21:45:49 -0400

    Homepage draft 2

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,16 +1,14 @@
 import { Link } from "react-router-dom"
+import LogoImage from "./images/dttc-logo.png"
 
 export const Navbar = () => {
   return <nav className="nav">
-    <div className="title-container">
-    <Link to ="/" className="site-title">Dead To The Core</Link>
-    <p id="title-sub">Boston based Grateful Dead Tribute</p>
-    </div>
+    <Link to ="/" className="site-title"><img id="logo-image" src={LogoImage} alt="logo" /></Link>
     <ul className="nav-links">
-            <Link to ="/band">Band</Link>
-            <Link to ="/tour">Tour</Link>
-            <Link to ="/gallery">Gallery</Link>
-            <Link to ="/contact">Contact</Link>
+      <Link to ="/band">Band</Link>
+      <Link to ="/tour">Tour</Link>
+      <Link to ="/gallery">Gallery</Link>
+      <Link to ="/contact">Contact</Link>
     </ul>
     </nav>
 }
