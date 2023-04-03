4a8b8aac0117df93c090f1f1bd932adea05baa65
Phil Todisco <philtodiscoglass@gmail.com>
2023-04-03 17:07:53 -0400

    Display API data

--- client/src/pages/Band.js
+++ client/src/pages/Band.js
@@ -1,21 +1,42 @@
 import axios from 'axios'
+import { useState, useEffect } from 'react'
 
+const TourDateTable = () => {
+  const [tourDateData, setTourDateData] = useState([])
 
-export default function Band() {
-  const getAllTourDates = () => {
-    // testing using randomuser api
+  useEffect(() => {
     axios.get('https://dttc-api.herokuapp.com/tourDates', {
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      }
+      headers: { 'Access-Control-Allow-Origin': '*' }
     })
     .then(res => {
-      console.log(res.data)
+      setTourDateData(res.data)
     }).catch(err => {
       console.log(err)
     })
-  }
+  }, [])
+
+  // Return some JSX that uses tourDateData
   return (
-    <button onClick={getAllTourDates}>Test</button>
+    <div>
+      {tourDateData.map(tourDate => (
+        <div className='tour-date-container'>
+          <p>{tourDate.date}</p>
+          <p>{tourDate.city}, {tourDate.state}</p>
+          <p>{tourDate.venue}</p>
+          <a href={tourDate.rsvp}>RSVP</a>
+          <a href={tourDate.ticket}>Tickets</a>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default function Band() {
+  // Use TourDateTable component
+  return (
+    <div>
+      <h1>Band Tour Dates</h1>
+      <TourDateTable />
+    </div>
   )
 }
