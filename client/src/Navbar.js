ce9fb40044befa5316cc1b60bdcacb6614cdf83a
Phil Todisco <philtodiscoglass@gmail.com>
2023-04-05 19:11:48 -0400

    Fix css bugs/redundant styles

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -4,6 +4,7 @@ import LogoImage from "./images/dttc-logo.png"
 export const Navbar = () => {
   return <nav className="nav">
     <Link to ="/" className="site-title"><img id="logo-image" src={LogoImage} alt="logo" /></Link>
+    {/* <Link to ="/" className="site-title">Dead To The Core</Link> */}
     <ul className="nav-links">
       <Link to ="/band">Band</Link>
       <Link to ="/tour">Tour</Link>
