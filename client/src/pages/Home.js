8cc995c520c2c3b71ca148324c683933cb6958a6
Phil Todisco <philtodiscoglass@gmail.com>
2023-04-12 22:24:05 -0400

    Front page overhaul

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,13 @@
-// import HeroImage from "../images/hero-image.png"
+import Hero from "../images/hero-image.png"
+import { Link } from "react-router-dom"
 
 export default function Home() {
   return <div className="home-page">
-    {/* <img id="hero-image" src={HeroImage} alt="grateful dead logo" img/> */}
+    <img id="hero-image" src={Hero} alt="grateful dead logo" img/>
+    <div className="main-text-container">
+      <h1 className="home-main-text">A Boston based tribute breaking the boundaries of the Grateful Dead's timeless catalog</h1>
+      <Link to ="/tour" id="main-text-btn">Upcoming Shows</Link>
+    </div>
   </div>
 }
 
