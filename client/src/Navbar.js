d9077dec5c91028efbf953d20254fa41c300ee9c
Phil Todisco <philtodiscoglass@gmail.com>
2023-04-17 19:56:32 -0400

    Start responsive nav bar

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,16 +1,32 @@
 import { Link } from "react-router-dom"
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 // import LogoImage from "./images/dttc-logo.png"
 
 export const Navbar = () => {
+  const navRef = useRef(null)
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav")
+  }
+
   return <nav className="nav">
-    {/* <Link to ="/" className="site-title"><img id="logo-image" src={LogoImage} alt="logo" /></Link> */}
     <Link to ="/" className="site-title">Dead To The Core</Link>
-    <ul className="nav-links">
+    <ul className="nav-links" ref={navRef}>
       <Link to ="/band">Band</Link>
       <Link to ="/tour">Tour</Link>
       <Link to ="/gallery">Gallery</Link>
       <Link to ="/contact">Contact</Link>
+      <button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
     </ul>
+    <button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
     </nav>
 }
-
