51ca87aea7e20e5b48afc5f1a9c3c2b7a31adc10
Phil Todisco <philtodiscoglass@gmail.com>
2023-04-17 20:11:51 -0400

    Add conditional nav link

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom"
 import { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-// import LogoImage from "./images/dttc-logo.png"
+import { useState } from "react";
+
 
 export const Navbar = () => {
+  const [expanded, setExpanded] = useState(false)
   const navRef = useRef(null)
 
   const showNavbar = () => {
     navRef.current.classList.toggle("responsive_nav")
+    setExpanded(!expanded)
   }
 
   return <nav className="nav">
     <Link to ="/" className="site-title">Dead To The Core</Link>
     <ul className="nav-links" ref={navRef}>
+      {expanded && <Link to="/">Home</Link>}
       <Link to ="/band">Band</Link>
       <Link to ="/tour">Tour</Link>
       <Link to ="/gallery">Gallery</Link>
