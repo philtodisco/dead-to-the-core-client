879c61990d08a649cf9d8b69445d23ad1e85f36f
Phil Todisco <philtodiscoglass@gmail.com>
2023-04-18 17:18:39 -0400

    Homepage responsivness update

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -3,39 +3,6 @@ import { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { useState } from "react";
 
-// export const Navbar = () => {
-//   const [expanded, setExpanded] = useState(false)
-//   const [isVisible, setIsVisible] = useState(true)
-//   const navRef = useRef(null)
-
-//   const showNavbar = () => {
-//     navRef.current.classList.toggle("responsive_nav")
-//     setExpanded(!expanded)
-//     setIsVisible(!isVisible)
-//   }
-
-//   return <nav className="nav">
-//     {isVisible && <Link to ="/" className="site-title">Dead To The Core</Link>}
-//     <ul className="nav-links" ref={navRef}>
-//       {expanded && <Link to="/">Home</Link>}
-//       <Link to ="/band">Band</Link>
-//       <Link to ="/tour">Tour</Link>
-//       <Link to ="/gallery">Gallery</Link>
-//       <Link to ="/contact">Contact</Link>
-//       <button
-// 					className="nav-btn nav-close-btn"
-// 					onClick={showNavbar}>
-// 					<FaTimes />
-// 				</button>
-//     </ul>
-//     {isVisible && <button
-// 				className="nav-btn"
-// 				onClick={showNavbar}>
-// 				<FaBars />
-// 			</button>}
-//     </nav>
-// }
-
 export const Navbar = () => {
 	const navRef = useRef();
   const [showLink, setShowLink] = useState(false)
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,12 @@ import Hero from "../images/hero-image.png"
 import { Link } from "react-router-dom"
 
 export default function Home() {
-  // return <div className="home-page">
-  //   <img id="hero-image" src={Hero} alt="grateful dead logo" img/>
-  //   <div className="main-text-container">
-  //     <h1 className="home-main-text">A Boston based tribute breaking the boundaries of the Grateful Dead's timeless catalog</h1>
-  //     <Link to ="/tour" id="main-text-btn">Upcoming Shows</Link>
-  //   </div>
-  // </div>
+  return <div className="home-page">
+    <img id="hero-image" src={Hero} alt="grateful dead logo" img/>
+    <div className="main-text-container">
+      <h1 className="home-main-text">A Boston based tribute breaking the boundaries of the Grateful Dead's timeless catalog</h1>
+      <Link to ="/tour" id="main-text-btn">Upcoming Shows</Link>
+    </div>
+  </div>
 }
 
