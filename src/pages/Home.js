298b5b50e6b898663ce8da60afff382442a99718
Phil Todisco <philtodiscoglass@gmail.com>
2024-02-19 16:29:19 -0500

    Fix typo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
   return <div className="home-page">
     <img id="hero-image" src={Hero} alt="grateful dead logo" img/>
     <div className="main-text-container">
-    <h1 className="home-main-text">A Boston based tribute breaking the boundaries of the Grateful Dead's timeless catalog</h1>
+    <h1 className="home-main-text">A Boston based tribute breaking the boundaries of the Grateful Dead's timeless catalog.</h1>
       <Link to ="/tour" id="main-text-btn">Upcoming Shows</Link>
     </div>
   </div>
