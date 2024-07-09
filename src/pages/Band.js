d3a5ccabd8a2b6280d2807295fc7a8a3cb0a30c6
Phil Todisco <philtodiscoglass@gmail.com>
2024-07-08 21:01:09 -0400

    Update copy

--- src/pages/Band.js
+++ src/pages/Band.js
@@ -4,7 +4,7 @@ export default function Band() {
   return <div className="band-page">
   <div className='band-page-text-container'>
     <h1>About the band</h1>
-    <p>Dead to the Core is Sam Bouve (Bass), Sean Galbraith (Guitar), Reilly Somach (Drums), and Phil Todisco (Guitar & Pedal Steel). This collective of North Shore original artists, songwriters, and producers came together in 2020 to honor the timeless catalog of the Grateful Dead with a fresh and innovative approach. Dead to the Core celebrates Jerry’s spirit with inspired playing, infused with unrestrained improvisations, and a real good time.
+    <p>Dead to the Core is Sam Bouve (Bass), Sean Galbraith (Guitar), and Phil Todisco (Guitar & Pedal Steel). This collective of North Shore original artists, songwriters, and producers came together in 2020 to honor the timeless catalog of the Grateful Dead with a fresh and innovative approach. Dead to the Core celebrates Jerry’s spirit with inspired playing, infused with unrestrained improvisations, and a real good time.
     </p>
   </div>
  <img id='band-photo-1' src={Bp1} alt="Grateful Dead tribute band performing live" img/>
